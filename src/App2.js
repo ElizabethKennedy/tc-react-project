2f6f04e7f171b8ad6c9a3663fcde31651dcf405d
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-11-15 14:10:13 -0800

    initial commit

---
+++ src/App2.js
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react'
+import React, {Component} from 'react'
+import Textrow from 'TextRow.js'
+export default class App2 extends React.Component {
+    constructor (props)
+        super (props);
+
+             
+}
+
+     render() {
+        return (
+            <div>App2</div>
+        )
+     }
+    }
+
+
+
+
+
+
+
+
+
+
+
