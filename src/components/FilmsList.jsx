cae95c3901ca3125473fcafb436b41b18b5b21ec
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-05 00:09:54 -0800

    lifecycles

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -1,6 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+
+export default class FilmsList extends Component  {
+
+constructor (props){
+    super(props);
+
+    this.state = {
+        list: []
+    }
+
+    this.getFilms = this.getFilms.bind(this);
+}
+
+
+
+getFilms = async function(){
+    try{
+        let res = await fetch('https://ghibliapi.herokuapp.com/films');
+        let resJson = await res.json();
+        this.setState(this.state.list = resJson);
+    } catch (e){
+        console.error(e);
+    }
+}
+
+componentDidMount(){
+    this.getFilms();
+}
+
+    render(){
+        return (
+        <ul>
+            {this.state.list.map((ele) => {
+               return <li key={ele.id}>{ele.title} <br /> {ele.description}</li>
+            })};
+        </ul>
+        );
+    };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useState, useEffect } from 'react';
 export default FilmsList;
-import App3 from './App3.jsx';
+
+
+class FilmsList {
+
+}
 
 function FilmsList (props){
 
@@ -20,13 +79,14 @@ useEffect(() => {
     getFilms();
 }, []);
 
-    
+
         return (
         <ul>
             {list.map((ele) => {
-               return <li key={ele.id}><h2>{ele.title}</h2> <br /> <img src={ele.image} alt="Movie Poster" className="filmsList-img" /> <br /> <span className='light-text'><i>{ele.description}</i></span> </li>  
+               return <li key={ele.id}><h2>{ele.title}</h2> <br /> <img src={ele.image} alt="Movie Poster" className="filmsList-img" /> <br /> <span className='light-text'><i>{ele.description}</i></span> </li>
             })};
-            
+
         </ul>
-        ); 
+        );
 }
+*/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App2.js';
+import App2 from './App2.js';
+import App from './App.js';
 // eslint-disable-next-line no-unused-vars
 import reportWebVitals from './reportWebVitals';
 
@@ -9,10 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <FilmsList/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
